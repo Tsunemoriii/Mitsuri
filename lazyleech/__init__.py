610dfd6a56fef9c903d682ee5eeaf32e68c626ca
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2025-05-08 10:51:15 +0530

    Update __init__.py

--- lazyleech/__init__.py
+++ lazyleech/__init__.py
@@ -4,15 +4,15 @@ import aiohttp
 from io import BytesIO, StringIO
 from pyrogram import Client
 
-API_ID = os.environ.get('API_ID')
-API_HASH = os.environ.get('API_HASH')
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+API_ID = os.environ.get('API_ID', '20628383')
+API_HASH = os.environ.get('API_HASH', '65a242463b8af9ba7b3c41d8de9738d1')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '1881338776:AAFeT4_llHPqIEAvK7gZr6Cs-cDqvTLQPo4')
 TESTMODE = os.environ.get('TESTMODE')
 TESTMODE = TESTMODE and TESTMODE != '0'
 
 EVERYONE_CHATS = os.environ.get('EVERYONE_CHATS')
 EVERYONE_CHATS = list(map(int, EVERYONE_CHATS.split(' '))) if EVERYONE_CHATS else [-1001378211961]
-ADMIN_CHATS = os.environ.get('ADMIN_CHATS')
+ADMIN_CHATS = os.environ.get('ADMIN_CHATS', '-1001252296278')
 ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else [441422215]
 ALL_CHATS = EVERYONE_CHATS + ADMIN_CHATS
 # LICHER_* variables are for @animebatchstash and similar, not required
@@ -32,6 +32,9 @@ LEECH_TIMEOUT = int(os.environ.get('LEECH_TIMEOUT', 300))
 ARIA2_SECRET = os.environ.get('ARIA2_SECRET', '')
 IGNORE_PADDING_FILE = os.environ.get('IGNORE_PADDING_FILE', '1')
 IGNORE_PADDING_FILE = IGNORE_PADDING_FILE and IGNORE_PADDING_FILE != '0'
+DB_URL = os.environ.get('DB_URL', 'mongodb+srv://utahheroku10:utahheroku10@cluster0.lt5bp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+NYAA_RSS_LINKS = os.environ.get('NYAA_RSS_LINKS', 'https://nyaa.si/?page=rss&c=0_0&f=0&u=Ember_Encodes')
+RSS_RECHECK_INTERVAL = os.environ.get('RSS_RECHECK_INTERVAL', '5')
 
 logging.basicConfig(level=logging.INFO)
 app = Client('lazyleech', API_ID, API_HASH, plugins={'root': os.path.join(__package__, 'plugins')}, bot_token=BOT_TOKEN, test_mode=TESTMODE, parse_mode='html', sleep_threshold=30)
